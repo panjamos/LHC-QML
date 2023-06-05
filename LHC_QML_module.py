ad0ccaec665083e144711e267ed8c16238432b44
Matt <matpan21@bergen.org>
2023-06-05 12:42:16 -0400

    continued organization of previous code

--- LHC_QML_module.py
+++ LHC_QML_module.py
@@ -4,20 +4,29 @@ import qiskit
 import sklearn
 import os
 import sys
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
 
-def load_data(signal_datapath, background_datapath):
+def load_data(signal_datapath, background_datapath, keys):
     #file with HToZZ in name is signal
-    signal = uproot.open(signal_datapath)
-    background = uproot.open(background_datapath)
-    return signal, background
+    signal_tree = uproot.open(signal_datapath)
+    background_tree = uproot.open(background_datapath)
+    signal_dict = {}
+    background_dict = {}
+
+    for key in keys:
+        signal_dict[key] = signal_tree[key].array(library="np")
+        background_dict[key] = background_tree[key].array(library="np")
+    return signal_dict, background_dict
 
 
 
 def format_data(signal, background, feature_keys):
     #changes the format of the data to be more like the old code
 
-    number_signal_events = len(signal['f_Z1mass'].array(library="np"))
-    number_background_events = len(background['f_Z1mass'].array(library="np"))
+    number_signal_events = len(signal['f_Z1mass'])
+    number_background_events = len(background['f_Z1mass'])
 
     print('# of signal events: ' + str(number_signal_events))
     print('# of background events: ' + str(number_background_events))
@@ -29,7 +38,7 @@ def format_data(signal, background, feature_keys):
     #puts each feature array in the data array along the first axis
     #all signal events followed by all background events
     for key in feature_keys:
-        event_data = np.append(signal[key].array(library='np'), background[key].array(library='np'))
+        event_data = np.concatenate((signal[key], background[key]))
         events_and_labels = np.append(events_and_labels, [event_data], axis=0)
     #NOTE: rewrite later --> It's uneccessary to put everything in one array just to split it back up before we return
 
@@ -66,7 +75,7 @@ def save_model(fit_result, save_folder):
     while os.path.exists(fit_filepath + '.npy'):
         fit_filepath = fit_filepath_default + str(fit_number)
         fit_number += 1
-        if fit_number == 100:
+        if fit_number == 500:
             sys.exit("filepath likely incorrect") 
     
     np.save(fit_filepath, fit_result.x)
@@ -80,3 +89,28 @@ def score_model(vqc, train_features, test_features, train_labels, test_labels, f
 
     print(f"Quantum VQC on the training dataset: {train_score_loaded:.5f}")
     print(f"Quantum VQC on the test dataset:     {test_score_loaded:.5f}")
+
+
+
+def plot_pairwise(compare_keys, signal, background):
+    #expects data in dicts with keys being the features and value being 1d array of data for events
+    feature_dict = {}
+    for key in compare_keys:
+        feature_dict[key] = np.concatenate((background[key],signal[key]))
+
+    df = pd.DataFrame(feature_dict)
+
+    event_labels = np.concatenate((np.full(len(background[key]),"background"),np.full(len(signal[key]),"signal")))
+
+    df["Event Type"] = pd.Series(event_labels)
+
+    plot = sns.pairplot(df, hue="Event Type", corner=True, palette = {'signal' : 'r', 'background' : 'b'},markers=["X", "."], diag_kws = dict(common_norm=False), plot_kws = dict(linewidth=0.2,alpha=0.75))
+    plot.fig.suptitle("Feature Comparison Plots")
+    plt.show()
+
+def plot_loss(losses):
+    plt.title("Loss During training")
+    plt.xlabel("Iteration")
+    plt.ylabel("Loss")
+    plt.plot(range(1,len(losses)+1), losses)
+    plt.show()
--- plot.py
+++ plot.py
@@ -11,6 +11,7 @@ from qiskit.algorithms.optimizers import COBYLA, SLSQP, SPSA
 
 signal_datapath = "D:\LHC-QML\data\output_VBF_HToZZTo4L_M-125_8TeV-powheg-pythia6.root:HZZ4LeptonsAnalysisReduced"
 background_datapath = "D:\LHC-QML\data\output_GluGluToZZTo4L_8TeV-gg2zz-pythia6.root:HZZ4LeptonsAnalysisReduced"
+save_folder = "D:/LHC-QML/fit results"
 
 choice_feature_keys = [
  'f_lept1_pt', 'f_lept1_eta', 'f_lept1_phi', 'f_lept1_pfx', 'f_lept2_pt',
@@ -23,22 +24,14 @@ choice_feature_keys = [
 
 seed = 123
 n_training_points = 500
-graph = False
 #decides if callback makes a graph at each step
 
 training_feature_indices = [14,21,22,23] #corresponds to f_Z2mass, f_pt4l, f_eta4l, f_mass4l
 #training_key_indices = range(len(choice_feature_keys))
 num_features = len(training_feature_indices)
 
-feature_map = ZZFeatureMap(feature_dimension=num_features, reps=1)
-sampler = Sampler()
-ansatz = EfficientSU2(num_qubits=num_features, reps=3)
-optimizer = SLSQP(maxiter=10)
-
-
-
 #loads data from files
-signal, background = lqm.load_data(signal_datapath,background_datapath)
+signal, background = lqm.load_data(signal_datapath, background_datapath, choice_feature_keys)
 
 #formats data for input into vqc
 features, labels = lqm.format_data(signal, background, choice_feature_keys)
@@ -55,42 +48,5 @@ train_features, test_features, train_labels, test_labels = train_test_split(
     features[:2*n_signal_events], labels[:2*n_signal_events], train_size=0.8, random_state=seed)
 
 
-
-losses = []
-times = []
-
-def callback(weights, step_loss):
-#function called at each step of vqc training
-#expected to only have these two inputs and return None
-    losses.append(step_loss)
-    times.append(time.time())
-    if graph:
-        ipy.display.clear_output(wait=True)
-        plt.title("Objective function value against iteration")
-        plt.xlabel("Iteration")
-        plt.ylabel("Objective function value")
-        plt.plot(range(1,len(losses)+1), losses)
-        plt.show()
-    print(f"Iteration {len(losses)} Training time: {round(times[-1]-times[-2],2)} seconds")
-    print(f"Loss: {step_loss:.4f}")
-
-vqc = VQC(
-    sampler=sampler,
-    feature_map=feature_map,
-    ansatz=ansatz,
-    optimizer=optimizer,
-    callback=callback,
-)
-
-start = time.time()
-times.append(start)
-vqc.fit(train_features[:n_training_points,training_feature_indices], train_labels[:n_training_points]) #training the vqc
-elapsed = time.time() - start
-
-print(f"Training time: {round(elapsed)} seconds")
-
-fit_result = vqc._fit_result
-save_folder = "D:/QML/fit results"
-
-lqm.save_model(fit_result, save_folder)
-lqm.score_model(vqc, train_features, test_features, train_labels, test_labels, training_feature_indices)
+compare_keys = ['f_Z1mass','f_pt4l', 'f_eta4l', 'f_mass4l']
+lqm.plot_pairwise(compare_keys, signal, background)
